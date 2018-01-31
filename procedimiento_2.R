5cfc3f0518d0b0393200407bbabb9f14a2f03e03
SantiagoBurone <santiagoburone@gmail.com>
2018-01-31 17:32:37 -0300

    Update procedimiento_2.R

--- procedimiento_2.R
+++ procedimiento_2.R
@@ -4,7 +4,7 @@ rm(list=ls())
 setwd("C:/Users/sburone/Documents/Base Enero")
 
 #Cargo base de actividades y formulario
-actividades<-read.csv("base_paso_1_con_fracasos.csv", sep=",")
+actividades<-read.csv("base_paso_1.csv", sep=",")
 formulario<-read.csv("C:/Users/sburone/Documents/Base Enero/Formularios/base_con_formulario.csv", sep=",")
 #Me quedo unicamente con los estudiantes que son PLAN 2012 PUROS
 actividades2<-actividades[which(actividades$ESTCI%in%formulario$ESTCI),]
@@ -14,5 +14,5 @@ basefinal<-merge(actividades2, formulario, by="ESTCI")
 
 rm("actividades", "actividades2", "formulario")
 
-write.csv(basefinal, "basefinal_con_fracasos.csv")
+write.csv(basefinal, "basefinal_con_formulario.csv")
 
