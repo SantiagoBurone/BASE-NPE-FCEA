90d2ecb486ad9d58f49c8a66d2d3cf9aad8e8455
SantiagoBurone <santiagoburone@gmail.com>
2018-07-12 11:14:22 -0300

    Update Procedimiento_2_v1.R

--- Procedimiento_2_v1.R
+++ Procedimiento_2_v1.R
@@ -1,17 +1,28 @@
+#######################################################
+###########################################################
+############     PROCEDIMIENTO 2.V1
+############        BASE NPE
+
+#Este programa MERGEA la base de actividades con la base de formulario estadistico 
+#####################################
+
 library(foreign)
 library(plyr)
 rm(list=ls())
-setwd("C:/Users/sburone/Documents/Base Enero")
+setwd("C:/Users/sburone/Documents/BASE NPE FINAL/Bases/auxiliares")
 
 #Cargo base de actividades y formulario
-actividades<-read.csv("base_paso_1_con_fracasos.csv", sep=",")
-formulario<-read.csv("C:/Users/sburone/Documents/Base Enero/Formularios/base_con_formulario_y_fracasos.csv", sep=",")
+actividades<-read.csv("base_paso_1_con_fracasos2.csv", sep=",")
+formulario<-read.dta("base_formulario_montevideo.dta")
+formulario$ESTCI<-formulario$CI
 #Me quedo unicamente con los estudiantes que son PLAN 2012 PUROS
+
 actividades2<-actividades[which(actividades$ESTCI%in%formulario$ESTCI),]
 formulario$dups<-duplicated(formulario$ESTCI)
 formulario<-formulario[which(formulario$dups==FALSE),]
+formulario$dups<-NULL
 basefinal<-merge(actividades2, formulario, by="ESTCI")
 
 rm("actividades", "actividades2", "formulario")
 
-write.csv(basefinal, "basefinal_con_fracasos.csv")
+write.dta(basefinal, "basefinal_con_fracasos_formulario.dta")
